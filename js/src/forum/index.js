3c1326658f47c6f94c488e8266ebd14fbc1118f6
Eddie Webbinaro <ollitech@gmail.com>
2022-03-20 08:27:18 -0400

    fix import in frontend and transpile production js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,10 +1,5 @@
-
-import { extend, override } from 'flarum/common/extend';
-import app from 'flarum/admin/app';
+import app from 'flarum/forum/app';
 
 app.initializers.add('webbinaro/flarum-affiliator', () => {
 
- 
-
-  
 });
